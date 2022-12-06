39fe6f8267c590374193538ca6f67bb2d56fa51b
pierre le rendu <pierre.lerendu@gmail.com>
2022-12-06 13:07:40 +0100

    Update header

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,33 +6,10 @@ import Head from 'next/head';
 export const isSSR = typeof window === 'undefined';
 
 export default function App({ Component, pageProps }: AppProps) {
-  /*
-  const router = useRouter();
-
-  const subdomainPaths = {
-    request: '/request',
-    company: '/company',
-  };
-
-  const isSubdomainRoute = () => {
-    const paths = Object.values(subdomainPaths).filter((path) => router.pathname.startsWith(path));
-    return !!paths.length;
-  };
-
-  if (!isSSR) {
-    if (window.location.host.includes('gift-list') && !isSubdomainRoute()) {
-      // show 404 or redirect somewhere else
-      return <div>404 - not found</div>;
-    }
-  }
-
-  // otherwise fallthrough to the normal Next.js return
-  */
-
   return (
     <>
       <Head>
-        <title>Gift List</title>
+        <title>Gift List 2</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       
