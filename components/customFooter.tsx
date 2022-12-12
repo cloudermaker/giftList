c797ea0192abbc35a1e0cacfc8e06938ea4af6f1
Pierre Le Rendu <plerendu@d-edge.com>
2022-12-12 13:55:00 +0100

    remove absolute for footer

--- components/customFooter.tsx
+++ components/customFooter.tsx
@@ -2,7 +2,7 @@ import Link from "next/link"
 
 export const CustomFooter = (): JSX.Element => {
     return (
-        <div className="absolute inset-x-0 bottom-0 h-16 bg-gray-200">
+        <div className="inset-x-0 bottom-0 h-16 bg-gray-200">
             <div className="mx-10 flex justify-between h-full items-center">
                 <Link href="mailto:haha@gmail.com">Contact us</Link>
 
